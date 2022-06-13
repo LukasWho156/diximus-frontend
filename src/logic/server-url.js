b3f5041805775cdef6101388ac1e6bd1aae2d6c8
LukasWho156 <luschuel@googlemail.com>
2022-06-13 08:03:05 +0200

    updated build path

--- src/logic/server-url.js
+++ src/logic/server-url.js
@@ -1,3 +1,3 @@
-const serverUrl = process.env.REACT_APP_SERVER_URL ?? "https://wise-lies-brake-178-4-184-77.loca.lt"; //'http://192.168.178.31:3000';
+const serverUrl = process.env.REACT_APP_SERVER_URL ?? 'http://localhost:3000';
 
 export default serverUrl;
